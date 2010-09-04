842bcdb65332a797cde2ef45be3cacb08b924d13
seungjin <seungjin@email.arizona.edu>
2010-09-03 18:10:12 -0700

    ..

--- app/root.rb
+++ app/root.rb
@@ -2,6 +2,12 @@
 require 'sinatra'
 require 'haml'
 
+get "/robots.txt" do
+	response.headers['Cache-Control'] = "public, max-age=86400"
+	content_type 'text/plain', :charset => 'utf-8'
+	"User-agent: *\nDisallow: /"
+end
+
 get '/' do
   #'aaaa'
   haml :"root/index"
